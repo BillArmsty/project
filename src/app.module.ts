915eb5ea19d8ce003a4666eb556056b2f57bfc53
BillArmsty <armsty9@gmail.com>
2025-03-17 22:23:22 +0300

    :wrench:Add updatedAt field to JournalEntry and implement login/register DTOs:wrench:

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,27 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-// import { PrismaModule } from './prisma/prisma.module';
 
 import { AuthModule } from './auth/auth.module';
 import { JournalModule } from './journal/journal.module';
 import { UserModule } from './users/users.module';
+import { AuthResolver } from './auth/auth.resolver';
+import { JwtStrategy } from './auth/strategy/jwt.strategy';
+import { LocalStrategy } from './auth/strategy/local.strategy';
+import { PrismaModule } from './prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    PrismaModule,
+    PassportModule.register({
+      session: false,
+      defaultStrategy: 'jwt',
+    }),
+    UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
@@ -28,6 +39,6 @@ import { UserModule } from './users/users.module';
     UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthResolver, LocalStrategy, JwtStrategy],
 })
 export class AppModule {}
