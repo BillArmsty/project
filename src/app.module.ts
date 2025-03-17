1b5fc847174df376ee0d6fd068bb68e7354b1e72
BillArmsty <armsty9@gmail.com>
2025-03-17 17:15:59 +0300

    :fire:Implement Auth, User, and Journal modules with corresponding entities, DTOs, and services:fire:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+// import { PrismaModule } from './prisma/prisma.module';
+// import { UserModule } from './user/user.module';
+// import { JournalModule } from './journal/journal.module';
+import { AuthModule } from './auth/auth.module';
+import { JournalModule } from './journal/journal.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      playground: false,
+      sortSchema: true,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    AuthModule,
+    JournalModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
